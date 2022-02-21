0cf4f0d19b355ab976e016db8e334f03c200d20d
devanshshukla99 <devanshshukla99@outlook.com>
2022-02-21 20:20:44 +0530

    fix stack pointer test

--- tests/test_instruction_sets.py
+++ tests/test_instruction_sets.py
@@ -15,7 +15,7 @@ def test_reset(controller):
         "BC": "0x00 0x00",
         "DE": "0x00 0x00",
         "HL": "0x00 0x00",
-        "SP": "0x00 0x00",
+        "SP": "0xffff",
         "PC": "0x0800",
     }
 
